aa4cda8c47cc272c975d41e91402d6c2c6db7b88
landanurm-droogcompanii <ls@droogc.ru>
2014-01-09 18:00:30 +0400

    added empty main_activity_2

---
+++ DroogCompanii/src/main/java/ru/droogcompanii/application/activity/main_activity_2/MainActivity2.java
@@ -0,0 +1,26 @@
+package ru.droogcompanii.application.activity.main_activity_2;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+import ru.droogcompanii.application.R;
+
+public class MainActivity2 extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_2);
+
+
+    }
+
+}
