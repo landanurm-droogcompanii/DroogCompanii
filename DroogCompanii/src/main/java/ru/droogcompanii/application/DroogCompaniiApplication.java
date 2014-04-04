226d3bf86fa2cc581d4d60217f68c80ff2d32a4c
landanurm-droogcompanii <ls@droogc.ru>
2014-04-04 17:59:23 +0400

    mini-mini-refactoring

--- DroogCompanii/src/main/java/ru/droogcompanii/application/DroogCompaniiApplication.java
+++ DroogCompanii/src/main/java/ru/droogcompanii/application/DroogCompaniiApplication.java
@@ -3,17 +3,12 @@ package ru.droogcompanii.application;
 import android.app.Application;
 import android.content.Context;
 
-import com.google.analytics.tracking.android.GoogleAnalytics;
-import com.google.analytics.tracking.android.Tracker;
-
 /**
  * Created by ls on 16.01.14.
  */
 public class DroogCompaniiApplication extends Application {
 
     private static Context context;
-    private static GoogleAnalytics mGa;
-    private static Tracker mTracker;
 
     public static Context getContext() {
         return DroogCompaniiApplication.context;
@@ -23,23 +18,5 @@ public class DroogCompaniiApplication extends Application {
     public void onCreate() {
         super.onCreate();
         DroogCompaniiApplication.context = getApplicationContext();
-
-        //initializeGa();
-    }
-
-    private void initializeGa() {
-        String trackingId = getApplicationContext().getString(R.string.ga_trackingId);
-
-        mGa = GoogleAnalytics.getInstance(this);
-        mTracker = mGa.getTracker(trackingId);
-
-    }
-
-    public static Tracker getGaTracker() {
-        return mTracker;
-    }
-
-    public static GoogleAnalytics getGaInstance() {
-        return mGa;
     }
 }
